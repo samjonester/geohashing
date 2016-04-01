70851c0e3961cef2fb8d0bf616508384e1bcc1e7
Sam Jones <sam@samljones.com>
2016-04-01 17:10:24 -0400

    creating geohash and dow functions

---
+++ spec/geohashSpec.js
@@ -0,0 +1,29 @@
+var geohash = require('../lib/geohash')
+
+describe('geohashing', function() {
+  var date = function() {return '2005-05-26'};
+  var dow = function() {return '10458.68'};
+
+  it('should create a new set of coordinates from lat, long, date, dow', function() {
+    var subject = geohash.geohashWithDateAndDow(date)(dow)
+
+    subject(37.421542, -122.085589, function(result) {
+      expect(result).toEqual({lat: 37.857713, lon: -122.544543});
+    });
+  });
+
+
+  it('should get the opening dow', function() {
+    var requester = {
+      get: function(url, callback) {
+        callback({query: {results: {quote: [{Open: 'foobar'}]}}});
+      }
+    }
+    
+    var subject = geohash.dowWithRequester(requester)
+
+    subject(function(dow) {
+      expect(dow).toEqual('foobar');
+    });
+  });
+});
---
+++ spec/worldExistsSpec.js
@@ -0,0 +1,5 @@
+describe('world exists', function() {
+  it('should be true!', function() {
+    expect(true).toBe(true)
+  });
+});
