63abc1547ceb7f1be8f33e6743c407b94c8b2929
Sam Jones <sam@samljones.com>
2016-04-03 17:00:51 -0400

    cleanup in geohashing after implementing dow

--- spec/geohashSpec.js
+++ spec/geohashSpec.js
@@ -1,11 +1,12 @@
 var geohash = require('../lib/geohash')
 
 describe('geohashing', function() {
-  var date = function() {return '2005-05-26'};
-  var dow = function() {return '10458.68'};
 
   it('should create a new set of coordinates from lat, long, date, dow', function() {
-    var subject = geohash.geohashWithDateAndDow(date)(dow)
+    var date = function() {return '2005-05-26'};
+    var dow = function(callback) { callback('10458.68')};
+
+    var subject = geohash.geohashWithDateAndDow(date, dow);
 
     subject(37.421542, -122.085589, function(result) {
       expect(result).toEqual({lat: 37.857713, lon: -122.544543});
