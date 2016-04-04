d778202c7b775a52a54168de470fa5d06352f75b
Sam Jones <sam@samljones.com>
2016-04-03 22:41:52 -0400

    completed geohashing algorithm

--- spec/geohashSpec.js
+++ spec/geohashSpec.js
@@ -3,14 +3,22 @@ var geohash = require('../lib/geohash')
 describe('geohashing', function() {
 
   it('should create a new set of coordinates from lat, long, date, dow', function() {
-    var date = function() {return '2005-05-26'};
-    var dow = function(callback) { callback('10458.68')};
+    var hexToDec = function(hex) {
+      if (hex === 'db9318c2259923d0') {
+        return 0.857713;
+      } else if (hex === '8b672cb305440f97') {
+        return 0.544544;
+      }
+    }
 
-    var subject = geohash.geohashWithDateAndDow(date, dow);
+    var inputLat = 37.421542;
+    var inputLong = -122.085589;
+    var date = '2005-05-26';
+    var dow = '10458.68';
 
-    subject(37.421542, -122.085589, function(result) {
-      expect(result).toEqual({lat: 37.857713, lon: -122.544543});
-    });
+    var subject = geohash.calculateGeohash(hexToDec);
+    
+    expect(subject(inputLat, inputLong, date, dow)).toEqual({lat: 37.857713, lon: -122.544544});
   });
 
 
@@ -36,4 +44,10 @@ describe('geohashing', function() {
 
     expect(subject()).toEqual('2016-01-01');
   });
+
+  it('should convert a hexidecimal string to a floating point number', function() {
+    var subject = geohash.hexToDec;
+
+    expect(subject('8b672cb305440f97')).toBeCloseTo(0.544544)
+  })
 });
