3413b275e34fcd11725c016cb990b46d00814bdd
Sam Jones <sam@genomoncology.com>
2016-04-04 14:54:54 -0400

    cleanup dow

--- spec/dowSpec.js
+++ spec/dowSpec.js
@@ -4,12 +4,12 @@ var dow = require('../lib/dow')
 
 describe('Dow', function() {
   it('should get the opening dow', function(done) {
-   var requester = function(url) {
-     return Promise.resolve({
-       body: JSON.stringify({query: {results: {quote: [{Open: 'foobar'}]}}})
-     });
-   } 
-    
+    var requester = function(url) {
+      return Promise.resolve({
+        body: JSON.stringify({query: {results: {quote: [{Open: 'foobar'}]}}})
+      });
+    } 
+
     var subject = dow.dowWithRequester(requester);
 
     subject().then(function(data) {
